efd14274dcf24c87bf4d60aa80dc705a45047903
DANIELNAHUN <nahuncf5@gmail.com>
2022-12-05 12:21:48 -0500

    first commit

---
+++ src/components/LoginUser.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <v-card class="mx-auto py-2" max-width="350">
+      <v-img src="https://www.bopapier.ch/sources/img/login.png" height="250">
+      </v-img>
+      <v-card-title> Inicio de Sesion </v-card-title>
+      <v-text-field
+        label="Usuario"
+        :rules="[rules.required]"
+        class="input-group--focused px-3"
+      >
+      </v-text-field>
+      <v-text-field
+        :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+        :rules="[rules.required, rules.min]"
+        :type="show ? 'text' : 'password'"
+        name="input-10-2"
+        label="Contraseña"
+        hint="At least 8 characters"
+        value=""
+        class="input-group--focused px-3"
+        @click:append="show = !show"
+      ></v-text-field>
+      <v-card-actions>
+        <v-btn text color="deep-purple accent-4"> Ingresar </v-btn>
+        <v-btn text color="blue darken-3"> Crear Usuario </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+      rules: {
+        required: (value) => !!value || "Required.",
+        min: (v) => v.length >= 8 || "Min 8 characters",
+        emailMatch: () => `The email and password you entered don't match`,
+      },
+    };
+  },
+};
+</script>
