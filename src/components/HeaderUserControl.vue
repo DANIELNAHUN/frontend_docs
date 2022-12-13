eb881cdd16f9fba648c369eec9556a91fdfd7641
DANIELNAHUN <nahuncf5@gmail.com>
2022-12-13 08:45:07 -0500

    user control

--- src/components/HeaderUserControl.vue
+++ src/components/HeaderUserControl.vue
@@ -1,12 +1,42 @@
 <template>
-    <v-tabs>
-    <v-tab>Item One</v-tab>
-    <v-tab>Item Two</v-tab>
-    <v-tab>Item Three</v-tab>
-  </v-tabs>
+  <v-toolbar color="black" dark>
+    <v-avatar size="45">
+      <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
+    </v-avatar>
+
+    <v-divider class="mx-4" vertical></v-divider>
+
+    <v-spacer></v-spacer>
+
+    <v-menu bottom left>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn icon color="yellow" v-bind="attrs" v-on="on">
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item>
+          <v-list-item-title>Click Me 1</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title>Click Me 2</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title>Click Me 3</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-title>Click Me 4</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-toolbar>
 </template>
 <script>
-    export default{
-        name:'HeaderUser'
-    }
+export default {
+  name: "HeaderUser",
+};
 </script>
